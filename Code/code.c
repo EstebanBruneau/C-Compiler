0d7842d9240288f3f3b0c14e997588341b0043bc
Este <esteban.bruneau77@gmail.com>
2024-11-03 23:34:10 +0100

    hi

--- Code/code.c
+++ Code/code.c
@@ -1,6 +1,3 @@
 int main() {
-    if (1 || 1) {
-        debug 1;
-    }
-    return 0;
+    debug 1;
 }
--- Output/code.asm
+++ Output/code.asm
@@ -6,15 +6,6 @@ resn 0
   .main
   resn 0
   push 1
-  push 1
-  or
-  jumpf l0
-  push 1
   dbg
-  jump l1
-.l0
-.l1
-  push 0
-  ret
   drop 0
   ret
